4366d6a61c3c6c2e342a2137107d35a370944860
Gautham P R <gauthampr18@gmail.com>
2021-03-09 23:33:28 +0530

    Minor Optimization

--- src/app.js
+++ src/app.js
@@ -26,7 +26,6 @@ class App extends React.Component{
             window.addEventListener("load", function() {
               navigator.serviceWorker
                 .register("/serviceWorker.js")
-                .then(res => console.log("service worker registered"))
                 .catch(err => console.log("service worker not registered", err))
             })
         }          
